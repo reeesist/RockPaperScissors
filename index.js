7b54c1fbe7d2b66495f6ae001f0b54c0b4a3d5aa
Lukas <lpovilaitis@yahoo.com>
2022-07-30 13:22:51 +0100

    randomized computer choice added

---
+++ index.js
@@ -0,0 +1,10 @@
+function getComputerChoice() {
+let choice = ['Rock', 'Paper', 'Scissors'];
+let random = choice[(Math.random() * choice.length) | 0];
+return random
+log.console(choice.length)
+}
+
+function playerSelection() {
+
+}
