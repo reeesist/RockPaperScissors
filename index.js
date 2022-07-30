cf9460db30dccb42a48d7834506bb9ca6437d5ff
Lukas <lpovilaitis@yahoo.com>
2022-07-30 18:49:47 +0100

    Added function for playing 1 round

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
+const choice = ['rock', 'paper', 'scissors'];
+
 function getComputerChoice() {
-let choice = ['Rock', 'Paper', 'Scissors'];
-let random = choice[(Math.random() * choice.length) | 0];
-return random
-log.console(choice.length)
+    return choice[(Math.random() * choice.length) | 0];
+    
 }
 
-function playerSelection() {
+// playerChoice = ""
+
+
+function playRound(playerChoice, computerSelection) {
+    // computerSelection = getComputerChoice()
+    playerChoice = playerChoice.toLowerCase()
+    if (playerChoice == computerSelection)
+    return "It's a tie"
+    else if (playerChoice == "rock" && computerSelection == "scissors")
+    return "You won, rock beats scissors"
+    else if (playerChoice == "scissors" && computerSelection == "paper")
+    return "You won, scissors beats paper"
+    else if (playerChoice == "paper" && computerSelection == "rock")
+    return "You won, paper beats rock"
+    else if (playerChoice == "rock" && computerSelection == "paper")
+    return "You lost, paper beats rock"
+    else if (playerChoice == "scissors" && computerSelection == "rock")
+    return "You lost, rock beats scissors"
+    else if (playerChoice == "paper" && computerSelection == "scissors")
+    return "You lost, scissors beats paper"
+    else
+     return "O.O"
+}
+
+const playerChoice = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerChoice, computerSelection))
+
+function game() {
 
 }
