7df6c2b289dbec9d0947e2b3870aca6aa4ed070d
Lukas <lpovilaitis@yahoo.com>
2022-08-05 17:20:35 +0100

    DONE

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ computerCount = 0
 
 
 function playRound(playerChoice, computerSelection) {
-    playerChoice = prompt("Start the game by picking among 'Rock, Paper, Scissors'").toLowerCase();
     computerSelection = getComputerChoice();
     result = ""
     if (playerChoice == computerSelection) {
@@ -19,29 +18,89 @@ function playRound(playerChoice, computerSelection) {
     } else if (playerChoice == "rock" && computerSelection == "scissors" ||
                 playerChoice == "scissors" && computerSelection == "paper" ||
                 playerChoice == "paper" && computerSelection == "rock") {
-                    playerCount += 1
-                   return result = `You won, ${playerChoice} beats ${computerSelection}`
+                playerCount += 1
+                return result = `You won, ${playerChoice} beats ${computerSelection}`
                 }
-                else {
-                    computerCount += 1
-                   return result = `You lost ${computerSelection} beats ${playerChoice}`
+      else {
+                computerCount += 1
+                return result = `You lost ${computerSelection} beats ${playerChoice}`
                 }
 }
 
 function whoWon() {
-    result = ""
-    if (playerCount > computerCount) {
-    return result = "You won"
-    } else if (computerCount > playerCount) {
-      return result = "You lost"
-    } else 
-    return result = "It's a tie"
+    if (playerCount > computerCount && playerCount == 5) {
+       finalResult.textContent = "You won!";
+       buttons.forEach((button) => {
+        button.disabled = true;
+       });
+    } else if (computerCount > playerCount && computerCount == 5) {;
+       finalResult.textContent = "You lost!";
+       buttons.forEach((button) => {
+        button.disabled = true;
+       }); 
+    }
+    else finalResult.textContent = ''
 }
 
-function game() {
-    for (i = 0; i < 5; i++) {
-    playRound()
-    console.log(playerCount)
-    console.log(computerCount) }
-    console.log(whoWon())
+const body = document.querySelector('body');
+
+    const rock = document.createElement('button');
+    rock.classList.add('rock');
+    rock.textContent = 'ROCK';
+    body.appendChild(rock);
+
+    const paper = document.createElement('button');
+    paper.classList.add('paper');
+    paper.textContent = 'PAPER';
+    body.appendChild(paper);
+
+    const scissors = document.createElement('button');
+    scissors.classList.add('scissors');
+    scissors.textContent = 'SCISSORS';
+    body.appendChild(scissors);
+
+const buttons = document.querySelectorAll('button.rock, button.scissors, button.paper');
+
+rock.addEventListener('click', () => {
+    playRound('rock')
+    score.textContent = `Players score: ${playerCount} Computer score: ${computerCount}`;
+    whoWon();
+})
+
+paper.addEventListener('click', () => {
+    playRound('paper');
+    score.textContent = `Players score: ${playerCount} Computer score: ${computerCount}`;
+    whoWon();
+})
+
+scissors.addEventListener('click', () => {
+    playRound('scissors');
+    score.textContent = `Players score: ${playerCount} Computer score: ${computerCount}`;
+    whoWon();
+})
+
+var score = document.createElement('div');
+score.classList.add('score');
+body.appendChild(score);
+
+var finalResult = document.createElement('div');
+finalResult.classList.add('result');
+body.appendChild(finalResult);
+
+const playAgain = document.createElement('button');
+playAgain.classList.add('again');
+playAgain.textContent = "Play again";
+body.appendChild(playAgain)
+
+function restart() {
+    buttons.forEach((button) => {
+        button.disabled = false;
+       });
+       playerCount = 0;
+       computerCount = 0;
+       score.textContent = "";
+       finalResult.textContent = "";
+       
 }
+
+playAgain.addEventListener('click', restart)
